284016866677b70b27a6865406707dee6ac37005
Daniel <alencar.png@gmail.com>
2025-02-01 19:28:18 -0300

    fix: added cors configuration

--- app/__init__.py
+++ app/__init__.py
@@ -3,21 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from config import Config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
+cors = CORS()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    app.url_map.strict_slashes = False
+    
+    cors.init_app(app, resources={
+        r"/api/*": {
+            "origins": ["http://localhost:4200"],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"],
+            "supports_credentials": True
+        }
+    })
 
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
 
     from app.models import productORM, userORM  
-
     from app.routes import product_bp
     from app.auth import auth_bp
 
